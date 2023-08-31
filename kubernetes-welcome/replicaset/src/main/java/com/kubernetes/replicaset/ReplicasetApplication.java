2d03e14a734696f43ff76244942955a3102910c8
Thanushree <thanushree@swayaan.com>
2023-08-31 14:59:24 +0530

    Initial projects added

---
+++ kubernetes-welcome/replicaset/src/main/java/com/kubernetes/replicaset/ReplicasetApplication.java
@@ -0,0 +1,13 @@
+package com.kubernetes.replicaset;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReplicasetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReplicasetApplication.class, args);
+	}
+
+}
